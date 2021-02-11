989bec9f466db12c338ec4e1130bd90742f29ed9
MeliodasJAJA <78351382+MeliodasJAJA@users.noreply.github.com>
2021-02-11 00:11:42 -0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -93,8 +93,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 ╭─〘🛑 𝗢𝗯𝗲𝗱𝗲𝗰𝗲 𝗹𝗮𝘀 𝗿𝗲𝗴𝗹𝗮𝘀 🛑〙
 │➤❌𝑷𝒓𝒐𝒉𝒊𝒃𝒊𝒅𝒐 𝒍𝒍𝒂𝒎𝒂𝒓 𝒂𝒍 𝒃𝒐𝒕📲
 │➤❌𝑷𝒓𝒐𝒉𝒊𝒃𝒊𝒅𝒐 𝒔𝒑𝒂𝒎 𝒂𝒍 𝒃𝒐𝒕☢
-│➤❌𝑵𝒐 𝒂𝒈𝒓𝒆𝒈𝒂𝒓 𝒂𝒍 𝒃𝒐𝒕 𝒂 𝒈𝒓𝒖𝒑𝒐𝒔 sin autorización♻
-│➤✅𝑺𝒖𝒔𝒄𝒓𝒊́𝒃𝒆𝒕𝒆 𝒂 𝒎𝒊 𝒄𝒂𝒏𝒂𝒍🙂
+│➤❌𝑵𝒐 𝒂𝒈𝒓𝒆𝒈𝒂𝒓 𝒂𝒍 𝒃𝒐𝒕 𝒂 𝒈𝒓𝒖𝒑𝒐𝒔 SIN AUTORIZACIÓN♻
 ╰────
 %readmore`
     let header = conn.menu.header || '╭─〘✨ %category 〙'
